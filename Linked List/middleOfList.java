4cfffd9c1d644334a19d8f1c5beb94704ffeaa82
Himanshu Jha <90416078+HIMANSHU-1504@users.noreply.github.com>
2023-09-10 00:38:20 +0530

    Update middleOfList.java

--- Linked List/middleOfList.java
+++ Linked List/middleOfList.java
@@ -44,5 +44,15 @@ class Solution {
             }
             return head;
         }
+
+        // Another approach 
+        // without finding length
+
+        ListNode fast = head, slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
